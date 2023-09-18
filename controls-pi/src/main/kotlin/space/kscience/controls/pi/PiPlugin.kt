8b6a6abd9245e07d53402f724ff6f9f6f7c47f09
Alexander Nozik <altavir@gmail.com>
2023-09-18 09:00:04 +0300

    Update to PiPlugin logic

--- controls-pi/src/main/kotlin/space/kscience/controls/pi/PiPlugin.kt
+++ controls-pi/src/main/kotlin/space/kscience/controls/pi/PiPlugin.kt
@@ -1,22 +1,46 @@
 package space.kscience.controls.pi
 
+import com.pi4j.Pi4J
+import space.kscience.controls.manager.DeviceManager
+import space.kscience.controls.ports.PortFactory
 import space.kscience.controls.ports.Ports
 import space.kscience.dataforge.context.AbstractPlugin
 import space.kscience.dataforge.context.Context
 import space.kscience.dataforge.context.PluginFactory
 import space.kscience.dataforge.context.PluginTag
 import space.kscience.dataforge.meta.Meta
+import space.kscience.dataforge.names.Name
+import space.kscience.dataforge.names.parseAsName
+import com.pi4j.context.Context as PiContext
 
 public class PiPlugin : AbstractPlugin() {
     public val ports: Ports by require(Ports)
+    public val devices: DeviceManager by require(DeviceManager)
 
     override val tag: PluginTag get() = Companion.tag
+
+    public val piContext: PiContext by lazy { createPiContext(context, meta) }
+
+    override fun content(target: String): Map<Name, Any> = when (target) {
+        PortFactory.TYPE -> mapOf(
+            PiSerialPort.type.parseAsName() to PiSerialPort,
+        )
+
+        else -> super.content(target)
+    }
+
+    override fun detach() {
+        piContext.shutdown()
+        super.detach()
+    }
 
     public companion object : PluginFactory<PiPlugin> {
 
         override val tag: PluginTag = PluginTag("controls.ports.pi", group = PluginTag.DATAFORGE_GROUP)
 
         override fun build(context: Context, meta: Meta): PiPlugin = PiPlugin()
+
+        public fun createPiContext(context: Context, meta: Meta): PiContext = Pi4J.newAutoContext()
 
     }
 }
