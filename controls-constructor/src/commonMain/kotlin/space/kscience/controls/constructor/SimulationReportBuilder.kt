2622f71de80e8c266fca93a0782f46301c26cd0b
altavir <altavir@gmail.com>
2025-09-28 19:30:15 +0300

    Modify report builder to be more useful. Fix dangling coroutines.

--- controls-constructor/src/commonMain/kotlin/space/kscience/controls/constructor/SimulationReportBuilder.kt
+++ controls-constructor/src/commonMain/kotlin/space/kscience/controls/constructor/SimulationReportBuilder.kt
@@ -1,52 +1,61 @@
 package space.kscience.controls.constructor
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Job
+import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import space.kscience.controls.api.PropertyChangedMessage
+import space.kscience.controls.time.ValueWithTime
 import space.kscience.controls.time.simulationDispatcher
-import space.kscience.dataforge.meta.MetaConverter
 import space.kscience.dataforge.names.Name
+import kotlin.coroutines.CoroutineContext
 import kotlin.time.Clock
+import kotlin.time.Instant
+
+internal data class StateChange<T>(
+    val state: DeviceState<T>,
+    val time: Instant,
+    val value: T,
+)
 
 public class SimulationReportBuilder(
-    public val scope: CoroutineScope,
+    parentScope: CoroutineScope,
     public val clock: Clock
-) {
+) : AutoCloseable, CoroutineScope {
 
-    internal val events: MutableList<PropertyChangedMessage> = mutableListOf<PropertyChangedMessage>()
+    override val coroutineContext: CoroutineContext =
+        parentScope.coroutineContext + Job(parentScope.coroutineContext[Job])
 
-    private val channel = Channel<PropertyChangedMessage>(10000)
+    internal val events = mutableListOf<StateChange<*>>()
 
-    private val dataPutJob = scope.launch {
+    private val channel = Channel<StateChange<*>>(10000)
+
+    private val dataPutJob = launch {
         for (message in channel) {
             events.add(message)
         }
     }
 
-    public suspend fun emit(change: PropertyChangedMessage) {
-        channel.send(change)
+    public suspend fun <T> emit(state: DeviceState<T>, value: T = state.value, time: Instant = clock.now()) {
+        channel.send(StateChange(state, time, value))
+    }
+
+    override fun close() {
+        cancel()
     }
 }
 
-public class SimulationReport(public val data: List<PropertyChangedMessage>)
+public class SimulationReport internal constructor(internal val data: List<StateChange<*>>)
+
+@Suppress("UNCHECKED_CAST")
+public fun <T> SimulationReport.forState(state: DeviceState<T>): List<ValueWithTime<T>> = data.filter {
+    it.state == state
+}.map {
+    ValueWithTime(it.value as T, it.time)
+}
 
 public fun <T> SimulationReportBuilder.collectState(
-    state: DeviceState<T>,
-    converter: MetaConverter<T>,
-    source: Name,
-    property: String
-): Job = state.useValue(scope) { value ->
-    emit(
-        PropertyChangedMessage(
-            time = clock.now(),
-            property = property,
-            value = converter.convert(value),
-            sourceDevice = source
-        )
-    )
+    state: DeviceState<T>
+): Job = state.useValue(this) { value ->
+    emit(state, value = value)
 }
 
 /**
@@ -55,7 +64,8 @@ public fun <T> SimulationReportBuilder.collectState(
 public suspend fun <M : Model> M.runSimulationWithReport(
     block: suspend M.(reportBuilder: SimulationReportBuilder) -> Unit
 ): SimulationReport = withContext(context.simulationDispatcher) {
-    val reportBuilder = SimulationReportBuilder(this, clock)
-    block(reportBuilder)
-    SimulationReport(reportBuilder.events)
+    SimulationReportBuilder(this, clock).use { reportBuilder ->
+        block(reportBuilder)
+        SimulationReport(reportBuilder.events)
+    }
 }
