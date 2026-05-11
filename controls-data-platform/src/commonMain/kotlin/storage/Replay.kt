76312dc5f4b3cc064ab995c9b53525c8e2d585fc
altavir <altavir@gmail.com>
2026-05-11 10:22:34 +0300

    Replay interface

--- controls-data-platform/src/commonMain/kotlin/storage/Replay.kt
+++ controls-data-platform/src/commonMain/kotlin/storage/Replay.kt
@@ -1,8 +1,29 @@
 package space.kscience.controls.dataplatform.storage
 
 import space.kscience.controls.api.DeviceMessageSource
+import kotlin.time.Instant
 
-public interface Replay: DeviceMessageSource {
 
-//    suspend fun
+/**
+ * An interface for replaying device messages or data from storage
+ */
+public interface Replay: DeviceMessageSource, AutoCloseable {
+
+    /**
+     * Start playback.
+     *
+     * @param from The time to start playback from. If null, playback starts from the beginning.
+     * @param useOriginalTime If true, use the original timestamp of the messages, otherwise use the current time.
+     * @param timeScale The scale factor for adjusting the playback speed.
+     */
+    public suspend fun play(
+        from: Instant? = null,
+        useOriginalTime: Boolean = false,
+        timeScale: Double = 1.0,
+    )
+
+    /**
+     * Stop playback
+     */
+    public suspend fun stop()
 }
